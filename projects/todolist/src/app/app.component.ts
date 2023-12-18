954dd234ef2ed390443dc71921321e5217812a90
aboubacar <sidikinabe10@gmail.com>
2023-12-18 11:22:58 +0100

    pour le devoir devoir de vendredi

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `<h1>Welcome</h1>`,
+  template: `
+    <div class="container">
+      <h1>Welcome</h1>
+      <ul *ngFor="let a of this.todoList; index as i">
+        <li *ngIf="!a.isCompleted">
+          <span>{{ a.title }}</span>
+        </li>
+      </ul>
+    </div>
+  `,
   styles: [''],
 })
 export class AppComponent {
-  title = 'todolist';
+  todoList = TODOS;
+  ableTodoList: Todo[] = [];
+
+  constructor() {
+    console.table(this.todoList);
+  }
+
+  selectTodo(id: number) {
+    return this.todoList[id];
+  }
+  /*showAbleTodo(list: Todo[]) {
+    list.map((todo: Todo) => {
+      if (!todo.isCompleted) {
+        this.ableTodoList.push(todo);
+      }
+    });
+  }*/
 }
