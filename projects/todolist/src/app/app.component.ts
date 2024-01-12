ba691b2e10efcbc8b645ef719dec9db72a2a5880
aboubacar <sidikinabe10@gmail.com>
2024-01-12 08:59:44 +0100

    Aboubacar commit TP 10

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,41 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TODOS } from './mock-todo';
-import { Todo } from './todo';
+import { todoComponent } from './todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   template: `
     <div class="container">
-      <h1>Welcome</h1>
-      <ul *ngFor="let a of this.todoList; index as i">
-        <li *ngIf="!a.isCompleted">
-          <span>{{ a.title }}</span>
-        </li>
-      </ul>
+      <todo-list />
     </div>
   `,
   styles: [''],
+  imports: [CommonModule, RouterOutlet, AppComponent, todoComponent],
 })
-export class AppComponent {
-  todoList = TODOS;
-  ableTodoList: Todo[] = [];
-
-  constructor() {
-    console.table(this.todoList);
-  }
-
-  selectTodo(id: number) {
-    return this.todoList[id];
-  }
-  /*showAbleTodo(list: Todo[]) {
-    list.map((todo: Todo) => {
-      if (!todo.isCompleted) {
-        this.ableTodoList.push(todo);
-      }
-    });
-  }*/
-}
+export class AppComponent {}
